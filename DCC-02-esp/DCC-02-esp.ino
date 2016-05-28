29e19d8097a59fcd3ec5501a0b31d9bf601fda7f
Luke Cyca <me@lukecyca.com>
2016-05-28 15:10:02 -0700

    Less blocking in loop; use libdcc

--- DCC-02-esp/DCC-02-esp.ino
+++ DCC-02-esp/DCC-02-esp.ino
@@ -1,13 +1,16 @@
+#include <ESP8266WiFi.h>
 
-// Define these in the credentials.hpp file
+// Define these in the config.h file
 //#define WIFI_SSID "yourwifi"
 //#define WIFI_PASSWORD "yourpassword"
-//#define HOSTNAME "data.example.com"
-//#define PATH "/write?db=<database>&u=<user>&p=<pass>"
-#include "credentials.h"
+//#define INFLUX_HOSTNAME "data.example.com"
+//#define INFLUX_PORT 8086
+//#define INFLUX_PATH "/write?db=<database>&u=<user>&p=<pass>"
+//#define WEBSERVER_USERNAME "something"
+//#define WEBSERVER_PASSWORD "something"
+#include "config.h"
 
-#define PORT 8086
-#define TIMEOUT 5000
+#define DEVICE_NAME "dcc02"
 
 #define GREEN_LED_PIN 5
 #define RED_LED_PIN 4
@@ -17,54 +20,16 @@
 byte sensorAddr[N_SENSORS][8] = {
   {0x28, 0xFF, 0x9C, 0x5C, 0x70, 0x14, 0x04, 0x4D}, //7
 };
+char * sensorNames[] = {
+  "temp",
+};
+#define UPLOAD_FREQ 10000
 
 
-#include <ESP8266WiFi.h>
-#include <OneWire.h>
+#include "libdcc/webserver.h"
+#include "libdcc/onewire.h"
+#include "libdcc/influx.h"
 
-OneWire  ds(ONE_WIRE_PIN);
-
-// Reads data from sensor addr `a` and puts the temperature in temp (in celsius)
-// Returns 1 on success, 0 on failure
-int readTemperature(byte a[8], float *temp) {
-  ds.reset();
-  ds.select(a);
-  ds.write(0xBE); // Read Scratchpad
-
-  byte data[9];
-  for (int i=0; i<9; i++) {
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  byte calculatedCRC = OneWire::crc8(data, 8);
-  if (calculatedCRC != data[8]) {
-    Serial.print("CRC Failed: ");
-    Serial.print(calculatedCRC, HEX);
-    Serial.print(" != ");
-    Serial.println(data[8], HEX);
-    return 0;
-  }
-
-  uint8_t reg_msb = data[1];
-  uint8_t reg_lsb = data[0];
-  uint16_t TReading = reg_msb << 8 | reg_lsb;
-
-  int SignBit, Whole, Fract;
-  SignBit = TReading & 0x8000;  // test most sig bit
-  if (SignBit) // negative
-	TReading = (TReading ^ 0xffff) + 1; // 2's comp
-
-  Whole = TReading >> 4;  // separate off the whole and fractional portions
-  Fract = (TReading & 0xf) * 100 / 16;
-
-  *temp = Whole + (TReading & 0xf) / 16.;
-  if (SignBit)
-      *temp *= -1;
-
-  os_printf("Temperature: %c%d.%d Celsius\r\n",SignBit ? '-' : ' ', Whole, Fract < 10 ? 0 : Fract);
-  return 1;
-}
 
 
 #define NTOKENS (6)
@@ -78,6 +43,7 @@ void handle_serial_line(char * line) {
   }
 
   // Change all space characters to NULL-terminators and assign pointers to each token
+  // (Here be dragons)
   char * token[NTOKENS];
   int j = 1;
   token[0] = line;
@@ -112,6 +78,8 @@ void handle_serial_line(char * line) {
   }
 }
 
+unsigned long lastUploadIteration;
+
 void setup() {
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(RED_LED_PIN, OUTPUT);
@@ -119,22 +87,18 @@ void setup() {
   digitalWrite(GREEN_LED_PIN, LOW);
 
   Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-}
+  server.on("/restart", handleRestart);
+  server.on("/status", handleStatus);
+  server.on("/sensors", handleSensors);
+  server.onNotFound(handleNotFound);
+  server.begin();
 
-#define SER_BUFFER_LENGTH (256)
-char ser_buffer[SER_BUFFER_LENGTH];
-int ser_buffer_index = 0;
-
-void loop() {
-  char fields[N_SENSORS+10][16];
-
-  // Instruct all ds18b20 devices to take a measurement
-  ds.reset();
-  ds.skip();
-  ds.write(0x44, 1); // Start conversion
-  delay(100);
-
+  // Initialize zeroth iteration
+  takeAllMeasurementsAsync();
+  lastUploadIteration = millis();
   l1_realPower = 0;
   l2_realPower = 0;
   l1_apparentPower = 0;
@@ -147,6 +111,16 @@ void loop() {
   l2_powerFactor = 0;
   l1_powerSamples = 0;
   l2_powerSamples = 0;
+}
+
+#define SER_BUFFER_LENGTH (256)
+char ser_buffer[SER_BUFFER_LENGTH];
+int ser_buffer_index = 0;
+
+WiFiClient client;
+
+void loop() {
+  server.handleClient();
 
   // Accumulate serial input into our buffer
   while (Serial.available() > 0) {
@@ -162,12 +136,23 @@ void loop() {
     yield();
   }
 
-  // ds18b20 need ~750ms to take measurement
-  delay(700);
+  // Copy HTTP client response to Serial
+  while (client.connected() && client.available()) {
+    Serial.print(client.readStringUntil('\r'));
+  }
 
+  digitalWrite(GREEN_LED_PIN, HIGH);
 
-  // Read each ds18b20 device individually
-  float temp[N_SENSORS] = {0.0};
+  // If we are NOT ready to do a sensor iteraction, return early
+  if (millis() < lastUploadIteration + UPLOAD_FREQ) {
+    return;
+  }
+
+  // Read sensors
+  float temp[N_SENSORS];
+  float accum = 0.0;
+  float numAccum = 0;
+  String sensorBody = String(DEVICE_NAME) + " uptime=" + String(millis()) + "i";
   for (int i=0; i<N_SENSORS; i++) {
     Serial.print("Temperature sensor ");
     Serial.print(i);
@@ -175,122 +160,58 @@ void loop() {
     if (readTemperature(sensorAddr[i], &temp[i])) {
       Serial.print(temp[i]);
       Serial.println();
-      dtostrf(temp[i], 1, 3, fields[i]);
+      sensorBody += String(",") + sensorNames[i] + "=" + String(temp[i], 3);
     } else {
-      strcpy(fields[i], "null");
+      temp[i] = NAN;
     }
-
-    delay(100);
   }
 
-  if (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(RED_LED_PIN, HIGH);
-    delay(1000);
-    Serial.println("Connecting to wifi...");
-    return;
+  // Instruct sensors to take measurements for next iteration
+  takeAllMeasurementsAsync();
+
+  // Compute energy metrics
+  // If there are powerSamples, divide the accumulators by the number of samples to report the average
+  if (l1_powerSamples) {
+    sensorBody += String(",l1_realPower=") + String(l1_realPower/l1_powerSamples, 3);
+    sensorBody += String(",l1_apparentPower=") + String(l1_apparentPower/l1_powerSamples, 3);
+    sensorBody += String(",l1_Vrms=") + String(l1_Vrms/l1_powerSamples, 3);
+    sensorBody += String(",l1_Irms=") + String(l1_Irms/l1_powerSamples, 3);
+    sensorBody += String(",l1_powerFactor=") + String(l1_powerFactor/l1_powerSamples, 3);
   }
-  digitalWrite(RED_LED_PIN, LOW);
-  digitalWrite(GREEN_LED_PIN, HIGH);
+  if (l2_powerSamples) {
+    sensorBody += String(",l2_realPower=") + String(l2_realPower/l2_powerSamples, 3);
+    sensorBody += String(",l2_apparentPower=") + String(l2_apparentPower/l2_powerSamples, 3);
+    sensorBody += String(",l2_Vrms=") + String(l2_Vrms/l2_powerSamples, 3);
+    sensorBody += String(",l2_Irms=") + String(l2_Irms/l2_powerSamples, 3);
+    sensorBody += String(",l2_powerFactor=") + String(l2_powerFactor/l2_powerSamples, 3);
+  }
 
-  WiFiClient client;
-  if (client.connect(HOSTNAME, PORT)) {
-    Serial.print("Connected to ");
-    Serial.print(HOSTNAME);
-    Serial.print(":");
-    Serial.println(PORT);
-    delay(50);
+  // Reset energy values for the next reading
+  l1_realPower = 0;
+  l2_realPower = 0;
+  l1_apparentPower = 0;
+  l2_apparentPower = 0;
+  l1_Vrms = 0;
+  l2_Vrms = 0;
+  l1_Irms = 0;
+  l2_Irms = 0;
+  l1_powerFactor = 0;
+  l2_powerFactor = 0;
+  l1_powerSamples = 0;
+  l2_powerSamples = 0;
 
+  Serial.println(sensorBody);
 
-    // If there are powerSamples, divide the accumulators by the number of samples to report the average
-    if (l1_powerSamples) {
-      dtostrf(l1_realPower/l1_powerSamples, 1, 3, fields[N_SENSORS]);
-      dtostrf(l1_apparentPower/l1_powerSamples, 1, 3, fields[N_SENSORS+1]);
-      dtostrf(l1_Vrms/l1_powerSamples, 1, 3, fields[N_SENSORS+2]);
-      dtostrf(l1_Irms/l1_powerSamples, 1, 3, fields[N_SENSORS+3]);
-      dtostrf(l1_powerFactor/l1_powerSamples, 1, 3, fields[N_SENSORS+4]);
-    } else {
-      // No powerSamples, so report null for all power fields
-      for (int i=N_SENSORS; i<N_SENSORS+5; i++) {
-        strcpy(fields[i], "null");
-      }
-    }
-    if (l2_powerSamples) {
-      dtostrf(l2_realPower/l2_powerSamples, 1, 3, fields[N_SENSORS+5]);
-      dtostrf(l2_apparentPower/l2_powerSamples, 1, 3, fields[N_SENSORS+6]);
-      dtostrf(l2_Vrms/l2_powerSamples, 1, 3, fields[N_SENSORS+7]);
-      dtostrf(l2_Irms/l2_powerSamples, 1, 3, fields[N_SENSORS+8]);
-      dtostrf(l2_powerFactor/l2_powerSamples, 1, 3, fields[N_SENSORS+9]);
-    } else {
-      // No powerSamples, so report null for all power fields
-      for (int i=N_SENSORS+5; i<N_SENSORS+10; i++) {
-        strcpy(fields[i], "null");
-      }
-    }
-
-
-
-    // Prepare JSON payload for InfluxDB
-    char body[512];
-    os_sprintf(
-      body,
-      "dcc02 temp=%s,l1_realPower=%s,l1_apparentPower=%s,l1_Vrms=%s,l1_Irms=%s,l1_powerFactor=%s,l2_realPower=%s,l2_apparentPower=%s,l2_Vrms=%s,l2_Irms=%s,l2_powerFactor=%s,uptime=%di",
-      fields[0],
-      fields[1],
-      fields[2],
-      fields[3],
-      fields[4],
-      fields[5],
-      fields[6],
-      fields[7],
-      fields[8],
-      fields[9],
-      fields[10],
-      millis()
-    );
-    Serial.print("Writing to ");
-    Serial.println(PATH);
-    Serial.println(body);
-    Serial.println(strlen(body));
-
-
-    // Make HTTP POST Request to InfluxDB
-    client.print("POST ");
-    client.print(PATH);
-    client.print(" HTTP/1.1\r\n");
-
-    client.print("Host: ");
-    client.print(HOSTNAME);
-    client.print(":");
-    client.print(PORT);
-    client.print("\r\n");
-
-    client.print("User-Agent: ESP8266 Arduino\r\n");
-
-    client.print("Connection: close\r\n");
-
-    client.print("Content-Length: ");
-    client.print(strlen(body));
-    client.print("\r\n");
-
-    client.print("\r\n");
-    client.print(body);
-
-    for (int i=0; i<TIMEOUT; i+=100) {
-      if (client.available()) {
-        break;
-      }
-      Serial.print(".");
-      delay(100);
-    }
-
-    while (client.available()) {
-      Serial.println(client.readStringUntil('\r'));
-    }
-
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Wifi connected to " + WiFi.SSID() + " IP:" + WiFi.localIP().toString());
+    client.connect(INFLUX_HOSTNAME, INFLUX_PORT);
+    postRequestAsync(sensorBody, client);
+    digitalWrite(RED_LED_PIN, LOW);
   } else {
+    Serial.println("Connecting to wifi...");
     digitalWrite(RED_LED_PIN, HIGH);
   }
-
   digitalWrite(GREEN_LED_PIN, LOW);
-  delay(10000);
+
+  lastUploadIteration = millis();
 }
